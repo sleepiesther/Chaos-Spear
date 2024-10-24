993f7eb1c27940ac04ee00a8a493928eaabbdb0f
Onaku <Onaku>
2024-10-24 02:15:05 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Chaos_Spear
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label7 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(86, 56);
+            button1.Name = "button1";
+            button1.Size = new Size(235, 115);
+            button1.TabIndex = 0;
+            button1.Text = "Attach";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(448, 43);
+            button2.Name = "button2";
+            button2.Size = new Size(303, 70);
+            button2.TabIndex = 1;
+            button2.Text = "Save Position";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(448, 145);
+            button3.Name = "button3";
+            button3.Size = new Size(303, 70);
+            button3.TabIndex = 2;
+            button3.Text = "Load Position";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.Location = new Point(77, 310);
+            label1.Name = "label1";
+            label1.Size = new Size(158, 32);
+            label1.TabIndex = 3;
+            label1.Text = "Saved X Pos:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label2.Location = new Point(77, 342);
+            label2.Name = "label2";
+            label2.Size = new Size(157, 32);
+            label2.TabIndex = 4;
+            label2.Text = "Saved Y Pos:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label3.Location = new Point(77, 373);
+            label3.Name = "label3";
+            label3.Size = new Size(157, 32);
+            label3.TabIndex = 5;
+            label3.Text = "Saved Z Pos:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label4.Location = new Point(448, 310);
+            label4.Name = "label4";
+            label4.Size = new Size(177, 32);
+            label4.TabIndex = 8;
+            label4.Text = "Current Z Pos:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label5.Location = new Point(448, 342);
+            label5.Name = "label5";
+            label5.Size = new Size(177, 32);
+            label5.TabIndex = 7;
+            label5.Text = "Current Y Pos:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label6.Location = new Point(447, 373);
+            label6.Name = "label6";
+            label6.Size = new Size(178, 32);
+            label6.TabIndex = 6;
+            label6.Text = "Current X Pos:";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label7.Location = new Point(447, 250);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 32);
+            label7.TabIndex = 9;
+            label7.Text = "Speed: ";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label7);
+            Controls.Add(label4);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Chaos Spear";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private Label label7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Windows.Forms;
+using Reloaded.Memory;
+using Reloaded.Memory.Sigscan;
+using SharpHook;
+using SharpHook.Native;
+
+namespace Chaos_Spear
+{
+    public partial class Form1 : Form
+    {
+        private bool attached = false;
+        private Process proc;
+
+        private int xcoordOff = 0x02993FE8;
+        private int timerOff = 0x02993FE8;
+
+        private IntPtr coordAddress, timerAddress;
+
+        private ExternalMemory gameMem;
+
+        nint coordAdd;
+        nint camCoordAdd;
+        nint timerAdd;
+
+        private float[] savedPos = new float[3];
+
+        float[] oldPos = {0,0,0};
+
+        float xSpeed, zSpeed;
+
+        private SimpleGlobalHook kbHook;
+        private Task task;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!attached)
+                {
+                    proc = Process.GetProcessesByName("SONIC_X_SHADOW_GENERATIONS").FirstOrDefault();
+
+                    if (proc == null)
+                    {
+                        MessageBox.Show("SXSG could not be found");
+                        return;
+                    }
+                    coordAddress = IntPtr.Add(proc.MainModule.BaseAddress, xcoordOff);
+                    timerAddress = IntPtr.Add(proc.MainModule.BaseAddress, timerOff);
+
+                    gameMem = new ExternalMemory(proc);
+
+                    attached = true;
+                    button1.Text = "Detach";
+
+                    timer1.Start();
+                    
+                    kbHook = new SimpleGlobalHook(true);
+                    kbHook.KeyPressed += handle_keys;
+                    task = kbHook.RunAsync();
+                }
+                else
+                {
+                    attached = false;
+                    button1.Text = "Attach";
+                    timer1.Stop();
+                    
+                    //kbHook.Dispose();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!attached)
+            {
+                MessageBox.Show("Attach program to SXSG first");
+                return;
+            }
+            gameMem.Read<nint>((nuint)coordAddress, out coordAdd);
+
+            coordAdd += 0x88;
+            gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+            coordAdd += 0x28;
+            gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+            coordAdd += 0x0;
+            gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+            coordAdd += 0x58;
+            gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+            coordAdd += 0x1A8;
+            gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+            gameMem.Read<float>((nuint)coordAdd + 0x80, out savedPos[0]);
+            gameMem.Read<float>((nuint)coordAdd + 0x84, out savedPos[1]);
+            gameMem.Read<float>((nuint)coordAdd + 0x88, out savedPos[2]);
+
+            gameMem.Read<float>((nuint)coordAdd + 0xD0, out xSpeed);
+            gameMem.Read<float>((nuint)coordAdd + 0xD8, out zSpeed);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!attached)
+            {
+                MessageBox.Show("Attach program to SXSG first");
+                return;
+            }
+
+            gameMem.Write<float>((nuint)coordAdd + 0x80, savedPos[0]);
+            gameMem.Write<float>((nuint)coordAdd + 0x84, savedPos[1]);
+            gameMem.Write<float>((nuint)coordAdd + 0x88, savedPos[2]);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //if is in a level
+                float[] curPos = new float[3];
+                float speedHorizontal;
+
+                gameMem.Read<nint>((nuint)coordAddress, out coordAdd);
+
+                coordAdd += 0x88;
+                gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+                coordAdd += 0x28;
+                gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+                coordAdd += 0x0;
+                gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+                coordAdd += 0x58;
+                gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+                coordAdd += 0x1A8;
+                gameMem.Read<nint>((nuint)coordAdd, out coordAdd);
+
+                gameMem.Read<float>((nuint)coordAdd + 0x80, out curPos[0]);
+                gameMem.Read<float>((nuint)coordAdd + 0x84, out curPos[1]);
+                gameMem.Read<float>((nuint)coordAdd + 0x88, out curPos[2]);
+
+                gameMem.Read<float>((nuint)coordAdd + 0xD0, out xSpeed);
+                gameMem.Read<float>((nuint)coordAdd + 0xD8, out zSpeed);
+
+                speedHorizontal = (float)Math.Round(Math.Sqrt(Math.Pow(xSpeed,2) + Math.Pow(zSpeed, 2)), 1);
+
+                label1.Text = "Saved X Pos: " + savedPos[0];
+
+                label2.Text = "Saved Y Pos: " + savedPos[1];
+
+                label3.Text = "Saved Z Pos: " + savedPos[2];
+
+                label4.Text = "Current X Pos: " + Math.Round(curPos[0], 1);
+                label5.Text = "Current Y Pos: " + Math.Round(curPos[1], 1);
+                label6.Text = "Current Z Pos: " + Math.Round(curPos[2], 1);
+                label7.Text = "Speed: " + speedHorizontal;
+
+                oldPos[0] = curPos[0];
+                oldPos[1] = curPos[1];
+                oldPos[2] = curPos[2];
+
+                gameMem.Read<nint>((nuint)timerAddress, out timerAdd);
+
+                timerAdd += 0x1B0;
+                gameMem.Read<nint>((nuint)timerAdd, out timerAdd);
+
+
+                timerAdd += 0x10;
+                gameMem.Read<nint>((nuint)timerAdd, out timerAdd);
+
+
+                timerAdd += 0x60;
+                gameMem.Read<nint>((nuint)timerAdd, out timerAdd);
+
+
+                timerAdd += 0xB0;
+                gameMem.Read<nint>((nuint)timerAdd, out timerAdd);
+
+                timerAdd += 0x20;
+                gameMem.Read<nint>((nuint)timerAdd, out timerAdd);
+
+                gameMem.Write<float>((nuint)timerAdd + 0x28, 99999);
+
+            }
+            catch(Exception exception) {
+                /*
+                timer1.Stop();
+                MessageBox.Show(exception.ToString());*/
+                //Nothing happens here LOOOOL
+            }
+            finally
+            {
+                
+            }
+
+        }
+      
+        private void handle_keys(Object sender, KeyboardHookEventArgs e)
+        {
+            if (attached)
+            {
+                KeyCode key = e.Data.KeyCode;
+                if(key == KeyCode.VcF9)
+                {
+                    button2_Click(sender, e);
+                }
+                else if(key == KeyCode.VcF10)
+                {
+                    button3_Click(sender,e);
+                }
+            }
+        }
+    }
+}
